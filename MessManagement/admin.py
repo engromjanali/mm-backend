62769a7c2143abfddeec6579cd8afed4008125d3
Payer Ahmed <payerahmedopu@gmail.com>
2026-07-29 00:31:20 +0600

    create notice model in messmanagement app

--- MessManagement/admin.py
+++ MessManagement/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Mess,MessSeason, MessMemberShip, MessMemberShipRequest, MessMemberShipInvitation
+from .models import Mess,MessSeason, MessMemberShip, MessMemberShipRequest, MessMemberShipInvitation, Notices
 
 admin.site.register(Mess)
 admin.site.register(MessSeason)
 admin.site.register(MessMemberShip)
 admin.site.register(MessMemberShipRequest)
 admin.site.register(MessMemberShipInvitation)
+admin.site.register(Notices)
--- MessManagement/models.py
+++ MessManagement/models.py
@@ -67,5 +67,15 @@ class MessMemberShipInvitation(models.Model):
 
 
 
+class Notices(models.Model):
+    mess = models.ForeignKey(Mess, on_delete=models.CASCADE, related_name='notices')
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+
 
     
